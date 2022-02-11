e2626a7ad6e4d0c9a27b02f07d34d8d83b7ab141
IWAGI <probel54@yandex.ru>
2022-02-11 15:56:21 +0700

    Initial Commit

---
+++ UIKitTutorials/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTutorials
+//
+//  Created by Mikhail on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
