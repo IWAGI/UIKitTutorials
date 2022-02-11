e6ffae3bed3cd57b70fa5adfdac525282e489ac2
IWAGI <probel54@yandex.ru>
2022-02-11 16:32:12 +0700

    Add UILabel functional.

--- UIKitTutorials/View Controllers/StartScreenViewController.swift
+++ UIKitTutorials/View Controllers/StartScreenViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ UIKitTutorials/View Controllers/UILabelViewController.swift
@@ -0,0 +1,44 @@
+//
+//  UILabelViewController.swift
+//  UIKitTutorials
+//
+//  Created by Mikhail on 11.02.2022.
+//
+
+import UIKit
+
+class UILabelViewController: UIViewController {
+
+    @IBOutlet weak var mainLable: UILabel!
+    
+   private let codeLabel: UILabel = {
+       let label = UILabel()
+       label.frame = CGRect(x: 24, y: 100, width: 100, height: 100)
+       
+       label.text = "Label from code!"
+       label.textColor = .black
+       label.numberOfLines = 0
+       label.textAlignment = .center
+
+       label.shadowColor = .white
+       label.shadowOffset = CGSize(width: 5, height: 5)
+       
+       label.layer.cornerRadius = 5
+       label.layer.backgroundColor = UIColor.lightGray.cgColor
+       label.layer.borderColor = UIColor.black.cgColor
+       label.layer.borderWidth = 2
+       
+       return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(codeLabel)
+    }
+    
+    @IBAction func closeButtonTapped() {
+        dismiss(animated: true)
+    }
+    
+}
